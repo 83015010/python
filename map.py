bb93c1cec29511ad4679678ec6a34ff4d43fb988
jiangmin <214536733@qq.com>
2017-05-11 11:07:46 +0800

    pycharm commit

---
+++ map.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+# 基础语法 - map 
+def delEmpty(s):
+    return s and s.strip()
+
+name ='my name is XXX and oa ffff ewrwrd'
+print('删除字符串中所有的空格:')
+print(list(filter(delEmpty,name)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
